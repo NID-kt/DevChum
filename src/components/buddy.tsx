7a31bc255b2eb4742a92bd5b338641f13aa5f965
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-07-06 05:36:51 +0000

    でぶちゃむがいなくなった時の画面のコードを実装

--- src/components/buddy.tsx
+++ src/components/buddy.tsx
@@ -5,8 +5,15 @@ const Buddy = () => {
         Your buddy
       </h1>
       <img src="./images/buddy/gif0047.gif" alt="buddy" className="w-60 h-60" />
+      
     </div>
   );
 };
 
 export default Buddy;
+
+// 死んでいる場合のコード(imgタグと挿し変える)
+{/* <div className="w-60 h-60 border border-gray-500 rounded-lg mt-8">
+  <p className="text-center text-l mt-4">でぶちゃむがいなくなっちゃった...😿</p>
+  <img src="./images/grave.png" alt="buddy" className="w-36 h-36 m-auto mt-8" />
+</div> */}
